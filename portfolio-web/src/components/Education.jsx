943930e5f13962c0a9d00a6a8f930e9b92e3d67e
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-12-10 14:37:27 +0530

    added

--- portfolio-web/src/components/Education.jsx
+++ portfolio-web/src/components/Education.jsx
@@ -1,16 +1,43 @@
+// Education.jsx
 import React from "react";
 import EducationCard from "../layouts/EducationCard";
 
 const Education = () => {
+  const educationData = [
+    {
+      title: "Bachelor of Technology",
+      degree: "Computer Science and Engineering",
+      duration: "June 2020 - Ongoing",
+      institute: "Vellore Institute of Technology - Bhopal",
+      grade: "8.19%",
+      certificate: "Certificate of Web Training",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus dolor incidunt quo, nulla dolore molestiae esse animi hic, placeat velit omnis quasi officia suscipit. Natus dignissimos reiciendis ut assumenda corrupti?",
+    },
+    {
+      title: "Class XII",
+      institute: "Shree G.K. Dholakiya School - Rajkot",
+      duration: "May 2019",
+      grade: "73.84%",
+    },
+    {
+      title: "Class X",
+      institute: "Shree P.V. MODI HIGH SCHOOL - Rajkot",
+      duration: "May 2017",
+      grade: "90.50%",
+    },
+  ];
+
   return (
-    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 bg-backgroundColor">
+    <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 bg-backgroundColor">
       <h1 className="text-center font-semibold text-4xl mt-24 lg:mt-16 mb-8 lg:mb-7">
         Education
       </h1>
 
-      <div className=" bg-white p-5 rounded-md">
-        <EducationCard title="Programming Course" />
-        <EducationCard title="Web developer courses" />
+      <div className="bg-white p-5 rounded-md">
+        {educationData.map((education, index) => (
+          <EducationCard key={index} {...education} />
+        ))}
       </div>
     </div>
   );
--- portfolio-web/src/layouts/EducationCard.jsx
+++ portfolio-web/src/layouts/EducationCard.jsx
@@ -1,24 +1,24 @@
+// EducationCard.jsx
 import React from "react";
 
 const EducationCard = (props) => {
   return (
-    <div className=" flex flex-col lg:flex-row py-4">
-      <div className=" w-full lg:w-2/4">
-        <h2 className=" font-semibold">{props.title}</h2>
+    <div className="flex flex-col lg:flex-row py-4">
+      <div className="w-full lg:w-2/4">
+        <h2 className="font-semibold">{props.title}</h2>
         <div className="mt-2">
-          <p>Student</p>
-          <p>Jan 1016 - Dec 2021</p>
+          {/* {props.degree && <p>{props.degree}</p>} */}
+          <p>{props.duration}</p>
+          {props.institute && <p>{props.institute}</p>}
+          {props.grade && <p>{props.grade}</p>}
         </div>
       </div>
       <div>
-        <h2 className=" font-semibold mt-2 lg:mt-0">Certificate of web training</h2>
-        <p className=" mt-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-          dolor incidunt quo, nulla dolore molestiae esse animi hic, placeat
-          velit omnis quasi officia suscipit. Natus dignissimos reiciendis ut
-          assumenda corrupti?
-        </p>
-        <div className=" bg-slate-500 w-full h-0.5 mt-2"></div>
+        <h2 className="font-semibold mt-2 lg:mt-0">{props.degree}</h2>
+        {props.description && (
+          <p className="mt-2">{props.description}</p>
+        )}
+        <div className="bg-slate-500 w-full h-0.5 mt-2"></div>
       </div>
     </div>
   );
