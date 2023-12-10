28a51bc4675804f821750b590efdda00b14ca448
Darshil Thummar <darshil.thummar2020@vitbhopal.ac.in>
2023-12-10 16:23:25 +0530

    modification

--- portfolio-web/src/components/Footer.jsx
+++ portfolio-web/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BsInstagram } from "react-icons/bs"; 
-import { FaFacebook, FaLinkedin} from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
@@ -13,13 +13,13 @@ const Footer = () => {
     <div className="flex flex-col lg:flex-row justify-between lg:px-32 px-5 bg-white p-5">
 
       <div className="flex gap-10">
-        <BsInstagram size={32} className="hover:text-brightColor transition-all cursor-pointer" onClick={() => handleClick('https://www.instagram.com')}/>
+        <BsInstagram size={32} className="hover:text-brightColor transition-all cursor-pointer" onClick={() => handleClick('https://instagram.com/smitpatel6534?igshid=NzZlODBkYWE4Ng%3D%3D&utm_source=qr')}/>
 
-        <FaFacebook size={32} className="hover:text-brightColor transition-all cursor-pointer" onClick={() => handleClick('https://www.facebook.com')} />
+        <FaGithub size={32} className="hover:text-brightColor transition-all cursor-pointer" onClick={() => handleClick('https://github.com/SMITP6875')} />
 
-        <FaLinkedin size={32} className="hover:text-brightColor transition-all cursor-pointer" onClick={() => handleClick('https://www.linkedin.com')} />
+        <FaLinkedin size={32} className="hover:text-brightColor transition-all cursor-pointer" onClick={() => handleClick('https://www.linkedin.com/in/patel-smit-685a531ba/')} />
         
-        <FaXTwitter size={32} className="hover:text-brightColor transition-all cursor-pointer" onClick={() => handleClick('https://discord.com')} />
+        <FaXTwitter size={32} className="hover:text-brightColor transition-all cursor-pointer" onClick={() => handleClick('https://twitter.com/PatelSm59587741')} />
       </div>
 
       <div className="flex flex-row items-center cursor-pointer mt-5 lg:mt-0">
--- portfolio-web/src/components/Home.jsx
+++ portfolio-web/src/components/Home.jsx
@@ -3,24 +3,34 @@ import img from "../assets/img/hero.png";
 import Button from "../layouts/Button";
 
 const Home = () => {
+
+  const handleDownloadCV = () => {
+    window.open('https://drive.google.com/file/d/1CVLink', '_blank');  
+  }
+
   return (
-    <div className=" min-h-screen flex flex-col justify-center lg:flex-row items-center lg:px-32 px-5 bg-backgroundColor">
+    <div className="min-h-screen flex flex-col justify-center lg:flex-row items-center lg:px-32 px-5 bg-backgroundColor">
+
       <div className="flex flex-col items-center text-center lg:text-start lg:items-start w-full lg:w-3/4 space-y-4">
-        <h1 className=" text-5xl font-semibold leading-tight mt-24">
-          I'm Alex Anderson
-          <span className=" text-brightColor"> Front-end </span>
-          Developer
+
+        <h1 className="text-5xl font-semibold leading-tight mt-24">
+          I'm Smit  
+          <span className="text-brightColor">Front-end</span> Developer  
         </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A
-          reprehenderit, quos praesentium iusto earum obcaecati nesciunt.
-          Corporis quasi ratione a.
-        </p>
-        <Button title="HIRE ME" />
+
+        <p>My Fascination With Technology...</p>
+
+        <a href="/posts">
+        <Button title="Download CV" onClick={handleDownloadCV} />
+        </a>
+        
+
       </div>
-      <div className=" mt-20">
+
+      <div className="mt-20">
         <img width={570} src={img} alt="img" />
       </div>
+
     </div>
   );
 };
